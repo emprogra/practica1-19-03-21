e23a9e234c3e6a7832c2f4942f07764a6fc04ef2
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-03-19 16:41:10 -0400

    Creando Modelo

---
+++ Practica5/ApiProducto/Models/Producto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ApiProducto.Models
+{
+    public enum Celulares
+    {
+        Apple,
+        Samsung,
+        Xiaomi,
+        Huawei,
+        Sony,
+
+    }
+    public class Producto
+    {
+        [Key]
+        [Required]
+        public int Codigo_de_Producto { get; set; }
+        [Required]
+        [StringLength(200, MinimumLength = 1, ErrorMessage = ("Ingrese codigo entre 1 a 200 caracteres"))]
+        public string Descripción { get; set; }
+        public string Foto { get; set; }
+        public Celulares Categoría { get; set; }
+        public double Precio { get; set; }
+        [DisplayFormat(DataFormatString ="{0:yyyy/MM/dd}")]
+        public DateTime Fecha_de_creación_del_producto { get; set; }
+
+
+    }
+}
